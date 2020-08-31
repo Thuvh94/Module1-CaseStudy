0f6c2ce9843ed57d65f609475a51944a04fcc270
Thuvh94 <aloha.hat.2508@gmail.com>
2020-08-31 21:18:54 +0700

    second commit

--- js/Gameboard.js
+++ js/Gameboard.js
@@ -1,45 +1,43 @@
 function GameBoard() {
     this.drawCharacterButton = function () {
-        let alphabet = "abcdefghijklmnopqrstuvwxyz";
+        let alphabet = [
+            ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+            ['i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'],
+            ['q', 'r', 's', 't', 'u', 'v', 'w', 'x'],
+            ['y','z'],
+        ]
         let string;
-        string = "<table>";
-        string += "<tr>";
+        string = `<table id='characterButton'>`;
         for (let i = 0; i < alphabet.length; i++) {
-            string += "<td><button>"+alphabet[i]+"</button></td>";
+            string+=`<tr>`;
+            for (let j = 0; j < alphabet[i].length; j++) {
+                string += `<td><button id="${i}-${j}" onclick="gameBoard.getClickCharacter('${i}-${j}')">${alphabet[i][j]}</button></td>`;
+            }
+            string+=`</tr>`;
         }
-        string += "</tr>";
-        string += "</table>";
-        document.write(string);
+        string += `</table>`;
+        return string;
     }
-
+// Đặt id cho table/ Cho td vào mảng. Làm hàm duyệt mảng lấy giá trị character.   getElementByTagName(td)
     this.drawAnswerArea = function (answer) {
         this.answer = answer.getAnswer();
         let table = "<table><tr>";
         for (let i = 0; i <this.answer.length; i++) {
-            table += "<td style='background-color: green; width:20px; height: 20px'></td>";
+            table += "<td style='background-color: green; width:20px; height: 20px' ></td>";
         }
         table += "</tr></table>";
-        document.write(table);
+        return table;
     }
-    this.checkTrueCharacter = function (character) {
-        answer = answer.getAnswer();
-        this.character = character;
-        if (answer.indexOf(character) < 0)
-            return false;
-        return true;
+    this.getClickCharacter = function (character) {
+        let char= document.getElementById(character).innerText;
+        return char;
     }
-    this.countFalse = function () {
-        let count =0;
-        if(!checkTrueCharacter){
-            while (count < 6){
-                count++;
-                drawHangMan(count);
-            }
-        }
-    }
-    this.drawHangMan = function () {
 
+    this.checkTrueCharacter = function (character) {
+        this.character = this.getClickCharacter();
+       alert(this.character);
     }
+
 
 
 
