401e975c22e10d59a334d7bc0fbd4d494b5f1ed5
Thuvh94 <aloha.hat.2508@gmail.com>
2020-09-01 17:26:52 +0700

    display true character

--- js/Gameboard.js
+++ js/Gameboard.js
@@ -4,16 +4,16 @@ function GameBoard() {
             ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
             ['i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'],
             ['q', 'r', 's', 't', 'u', 'v', 'w', 'x'],
-            ['y','z'],
+            ['y', 'z'],
         ]
         let string;
         string = `<table id='characterButton'>`;
         for (let i = 0; i < alphabet.length; i++) {
-            string+=`<tr>`;
+            string += `<tr>`;
             for (let j = 0; j < alphabet[i].length; j++) {
-                string += `<td ><button class="button" id="${i}-${j}" onclick="gameBoard.drawHangman('${i}-${j}',answer)">${alphabet[i][j]}</button></td>`;
+                string += `<td ><button class="button" id="${i}-${j}" onclick="gameBoard.navigate('${i}-${j}',answer)">${alphabet[i][j]}</button></td>`;
             }
-            string+=`</tr>`;
+            string += `</tr>`;
         }
         string += `</table>`;
         return string;
@@ -21,11 +21,11 @@ function GameBoard() {
 
     this.drawAnswerArea = function (answer) {
         this.answer = answer.getAnswer();
-        let table = "<table><tr>";
-        for (let i = 0; i <this.answer.length; i++) {
-            table += "<td style='background-color: green; width:20px; height: 20px' ></td>";
+        let table = `<table><tr>`;
+        for (let i = 0; i < this.answer.length; i++) {
+            table += `<td id="${i}" style='background-color: green; width:20px; height: 20px' ></td>`;
         }
-        table += "</tr></table>";
+        table += `</tr></table>`;
         return table;
     }
     this.getClickCharacter = function (id) {
@@ -35,24 +35,41 @@ function GameBoard() {
     this.checkTrueCharacter = function (id, answer) {
         this.character = this.getClickCharacter(id);
         this.answer = answer.getAnswer();
-        if (this.answer.indexOf(this.character) >= 0){
+        if (this.answer.indexOf(this.character) >= 0) {
             return true;
         }
     }
-    this.countFalse = function (id,answer) {
-            if (!this.checkTrueCharacter(id,answer)){
-                this.count++;
-        } return this.count;
+    this.countFalse = function (id, answer) {
+        if (!this.checkTrueCharacter(id, answer)) {
+            this.count++;
+        }
+        return this.count;
     }
     this.count = 0;
-    this.displayTrueCharacter = function(){
-
+    this.displayTrueCharacter = function (id, answer) {
+        let char= document.getElementById(id).innerText;
+        for(i=0; i< answer.answer.length; i++){
+            if(char==answer.answer[i]){
+                document.getElementById(i).innerText= char;
+            }
+        }
     }
-    this.drawHangman = function (id,answer) {
-        this.count = this.countFalse(id,answer);
-        document.getElementById('hangManImg').attributes[1].value="../assets/Hangman-"+this.count+".png";
+    this.drawHangman = function (id, answer) {
+        this.count = this.countFalse(id, answer);
+        document.getElementById('hangManImg').attributes[1].value = "../assets/Hangman-" + this.count + ".png";
     }
-    // this.displayFalseCharacter =function () {
-    //
-    // }
+    this.navigate = function (id, answer) {
+        if (this.checkTrueCharacter(id,answer))
+            this.displayTrueCharacter(id,answer)
+        else {
+            this.drawHangman(id,answer);
+            this.displayFalseCharacter(id);
+        }
+    }
+    this.displayFalseCharacter =function (id) {
+        let string ="";
+        string += this.getClickCharacter(id);
+        return string;
+    }
 }
+
